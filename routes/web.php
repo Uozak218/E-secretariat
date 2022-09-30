c746f04277f9c2844d364a296806fd23924134a2
leobasthene.lima <leobasthene.lima@epitech.eu>
2022-09-30 13:54:21 +0100

    add

--- routes/web.php
+++ routes/web.php
@@ -14,64 +14,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-/*Route::get('Enregistrer', [CourrierController::class, 'Create'])->name('new_courrier');
-
-Route::get('save_new_courrier', [CourrierController::class, 'Store']);
-
-Route::get('Rechercher', function () {
-    return view('seecourrier')->with('courrier_arr', courrier::all());
-});
-
-Route::get('Rediger', function () {
-    return view('rediger')->with('courrier_arr', courrier::all());
-});
-
-Route::get('delete_courrier/{id}', [CourrierController::class, 'destroy']);
-
-Route::get('edit_courrier/{id}', [CourrierController::class, 'edit']);
-
-Route::get('update_courrier/{id}', [CourrierController::class, 'update']);
-
-Route::get('searchcourrier', [CourrierController::class, 'search'])->name('courrier.search');
-
-
-
-Route::get('EnregistrerContact', [ContactController::class, 'Create'])->name('new_contact');
-
-Route::get('save_new_contact', [ContactController::class, 'Store']);
-
-Route::get('RechercherContact', function () {
-    return view('seecontact')->with('contact_arr', contact::all());
-});
-
-Route::get('delete_contact/{id}', [ContactController::class, 'destroy']);
-
-Route::get('edit_contact/{id}', [ContactController::class, 'edit']);
-
-Route::get('update_contact/{id}', [ContactController::class, 'update']);
-
-Route::get('searchcontact', [ContactController::class, 'search'])->name('contact.search');
-
-
-
-Route::get('EnregistrerCollaborateur', [CollaborateurController::class, 'Create'])->name('new_partner');
-
-Route::get('save_new_collaborateur', [CollaborateurController::class, 'Store']);
-
-Route::get('RechercherCollaborateur', function () {
-    return view('seepartner')->with('collaborateur_arr', collaborateur::all());
-});
-
-Route::get('delete_partner/{id}', [CollaborateurController::class, 'destroy']);
-
-Route::get('edit_partner/{id}', [CollaborateurController::class, 'edit']);
-
-Route::get('update_partner/{id}', [CollaborateurController::class, 'update']);
-
-Route::get('search', [CollaborateurController::class, 'search'])->name('partner.search');
-
-*/
-
 Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('private', function () {
         return 'Bonjour Admin';
